7356737a7d7e57c2d621cd3501b0305643173f8f
Naureen <naureen.imran123@yahoo.com>
2023-02-20 17:24:17 +0000

    plot series of point

--- Projects/Data-Visualization/scatter_squares.py
+++ Projects/Data-Visualization/scatter_squares.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
 ax.set_title('Square Number', fontsize=24)
 ax.set_xlabel('Value', fontsize=14)
 ax.set_ylabel('Square of Value', fontsize=14)
