a693b8f4636439dfa5c86cc4168e2e9509ce973f
Naureen <naureen.imran123@yahoo.com>
2023-02-20 17:04:28 +0000

    change label type and line thickness

--- Projects/Data-Visualization/mpl_squares.py
+++ Projects/Data-Visualization/mpl_squares.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+ax.set_title('Square Number', fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
 plt.show()
