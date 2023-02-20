b0e25cdb1b6e8fd4316e052084889398a79db284
Naureen <naureen.imran123@yahoo.com>
2023-02-20 17:10:16 +0000

    correcting the plot

--- Projects/Data-Visualization/mpl_squares.py
+++ Projects/Data-Visualization/mpl_squares.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 ax.set_title('Square Number', fontsize=24)
 ax.set_xlabel('Value', fontsize=14)
 ax.set_ylabel('Square of Value', fontsize=14)
