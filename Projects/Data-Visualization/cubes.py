0b7211e6db959ab2675dcdccd0b5eeb75ea163ae
Naureen <naureen.imran123@yahoo.com>
2023-02-21 11:04:36 +0000

    change the color to red

--- Projects/Data-Visualization/cubes.py
+++ Projects/Data-Visualization/cubes.py
@@ -5,7 +5,7 @@ y_values = [x**3 for x in x_values]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.plot(x_values, y_values)
+ax.plot(x_values, y_values, c='red')
 ax.set_title("Cube Numbers")
 ax.set_xlabel('Values')
 ax.set_ylabel('Cube of the value')
