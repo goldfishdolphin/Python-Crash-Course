eac9ea32187f8b9dfe42ef21983f80e9564d3d8f
Naureen <naureen.imran123@yahoo.com>
2023-02-21 10:14:03 +0000

    calculate data automatically and define custom colours .

--- Projects/Data-Visualization/scatter_squares.py
+++ Projects/Data-Visualization/scatter_squares.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [pow(x, 2) for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.turbo, s=10)
 ax.set_title('Square Number', fontsize=24)
 ax.set_xlabel('Value', fontsize=14)
 ax.set_ylabel('Square of Value', fontsize=14)
+ax.axis([0, 1100, 0, 1100000])
 plt.show()
