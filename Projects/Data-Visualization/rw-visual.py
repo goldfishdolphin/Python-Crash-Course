79093660fd468367066a7ac8b34746fe734bf6fb
Naureen <naureen.imran123@yahoo.com>
2023-02-22 11:17:49 +0000

    plot starting and ending points

--- Projects/Data-Visualization/rw-visual.py
+++ Projects/Data-Visualization/rw-visual.py
@@ -8,6 +8,10 @@ while True:
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers,
                cmap=plt.cm.Blues, edgecolors='none')
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1],
+               c='red', edgecolors='none', s=100)
+
     plt.show()
     keep_running = input('Make another walk? (y/n): ')
     if keep_running == 'n':
