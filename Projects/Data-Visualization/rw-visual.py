891a6f0a331e129ab0c8aa88f56525b51aa89e3c
Naureen <naureen.imran123@yahoo.com>
2023-02-22 11:28:14 +0000

    add plot points to class

--- Projects/Data-Visualization/rw-visual.py
+++ Projects/Data-Visualization/rw-visual.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 from random_walk import Randomwalk
 while True:
-    rw = Randomwalk()
+    rw = Randomwalk(50_000)
     rw.fill_walk()
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers,
-               cmap=plt.cm.Blues, edgecolors='none')
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
+               cmap=plt.cm.Blues, edgecolors='none', s=1)
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1],
                c='red', edgecolors='none', s=100)
