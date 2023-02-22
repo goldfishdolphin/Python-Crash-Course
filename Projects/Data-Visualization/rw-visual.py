ad91e08f59e9b742bda848bbcd47e5cdf76e318f
Naureen <naureen.imran123@yahoo.com>
2023-02-22 11:10:43 +0000

    generate  multiple random walk

--- Projects/Data-Visualization/rw-visual.py
+++ Projects/Data-Visualization/rw-visual.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 from random_walk import Randomwalk
-rw = Randomwalk()
-rw.fill_walk()
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15, c=rw.y_values)
-plt.show()
+while True:
+    rw = Randomwalk()
+    rw.fill_walk()
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers,
+               cmap=plt.cm.Blues, edgecolors='none')
+    plt.show()
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
