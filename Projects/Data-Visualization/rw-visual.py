0537b06220d962659e190ed32270fad6ee6ecceb
Naureen <naureen.imran123@yahoo.com>
2023-02-22 11:31:52 +0000

    alter the size of plot to fill the screen

--- Projects/Data-Visualization/rw-visual.py
+++ Projects/Data-Visualization/rw-visual.py
@@ -4,7 +4,7 @@ while True:
     rw = Randomwalk(50_000)
     rw.fill_walk()
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9))
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
                cmap=plt.cm.Blues, edgecolors='none', s=1)
