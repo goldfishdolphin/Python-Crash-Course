87be45a22063a161e2e5c4f2294227737a253d8b
Naureen <naureen.imran123@yahoo.com>
2023-02-23 12:26:43 +0000

    create one d6 and one d10 dice and plot the results

--- Projects/Data-Visualization/die_visual.py
+++ Projects/Data-Visualization/die_visual.py
@@ -1,12 +1,12 @@
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
 from die import Die
-# Create two D6 dice.
+# Create a D6 and D10.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll()+die_2.roll()
     results.append(result)
 
@@ -22,6 +22,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 1000 times',
+my_layout = Layout(title='Results of rolling one D6 and one D10 50,000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
