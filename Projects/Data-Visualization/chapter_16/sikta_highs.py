4644152aea5462141e3c4a6fb6c03e84868581d6
Naureen <naureen.imran123@yahoo.com>
2023-02-24 14:27:50 +0000

    extract and read data

--- Projects/Data-Visualization/chapter_16/sikta_highs.py
+++ Projects/Data-Visualization/chapter_16/sikta_highs.py
@@ -3,5 +3,9 @@ filename = 'data/sikta_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # Get high temprature from this file
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+print(highs)
