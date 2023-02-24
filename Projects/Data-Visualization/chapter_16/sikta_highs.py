7ec2adedc20dd2a826db18091c63f2aa64f78e49
Naureen <naureen.imran123@yahoo.com>
2023-02-24 15:25:48 +0000

    plot the temp data

--- Projects/Data-Visualization/chapter_16/sikta_highs.py
+++ Projects/Data-Visualization/chapter_16/sikta_highs.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 filename = 'data/sikta_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -9,3 +10,13 @@ with open(filename) as f:
         high = int(row[5])
         highs.append(high)
 print(highs)
+# Plot high tempratures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+# Format plot
+ax.set_title('Daily high tempratures, July 2018', fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('Temprature (F)', fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
