9d21fda62799db430a37e6891fe21f786392601f
Naureen <naureen.imran123@yahoo.com>
2023-02-24 15:47:27 +0000

    plot temp and date

--- Projects/Data-Visualization/chapter_16/sikta_highs.py
+++ Projects/Data-Visualization/chapter_16/sikta_highs.py
@@ -1,22 +1,27 @@
 import csv
+from datetime import datetime
 import matplotlib.pyplot as plt
-filename = 'data/sikta_weather_07-2018_simple.csv'
+filename = 'Projects/Data-Visualization/chapter_16/data/sikta_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    # Get high temprature from this file
-    highs = []
+
+    # Get date and high temprature from this file
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
-print(highs)
+print(dates)
 # Plot high tempratures.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 # Format plot
 ax.set_title('Daily high tempratures, July 2018', fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('Temprature (F)', fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
