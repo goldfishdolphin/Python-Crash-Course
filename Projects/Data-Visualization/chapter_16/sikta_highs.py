e52990f1eea2adaaa7647b38878f5b92b6414f4d
Naureen <naureen.imran123@yahoo.com>
2023-02-24 15:56:19 +0000

    plot a second data series

--- Projects/Data-Visualization/chapter_16/sikta_highs.py
+++ Projects/Data-Visualization/chapter_16/sikta_highs.py
@@ -6,20 +6,23 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get date and high temprature from this file
-    dates, highs = [], []
+    # Get date,high and low temprature from this file
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 print(dates)
-# Plot high tempratures.
+# Plot high tempratures, highs and lows.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 # Format plot
-ax.set_title('Daily high tempratures, July 2018', fontsize=24)
+ax.set_title('Daily high, lowtempratures, July 2018', fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temprature (F)', fontsize=16)
