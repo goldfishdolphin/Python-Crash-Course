98ce2467b8e411d73a88695bd7fa1dcf4f19dd10
Naureen <naureen.imran123@yahoo.com>
2023-02-24 14:20:27 +0000

    print header and their positions after parsing the csv file headers

---
+++ Projects/Data-Visualization/chapter_16/sikta_highs.py
@@ -0,0 +1,7 @@
+import csv
+filename = 'data/sikta_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
