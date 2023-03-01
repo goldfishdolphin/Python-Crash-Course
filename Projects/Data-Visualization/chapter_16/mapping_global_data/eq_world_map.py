a52404b0040373f9a37dc08c5eb29219122e49a6
Naureen <naureen.imran123@yahoo.com>
2023-03-01 11:13:41 +0000

    Alternative way to specify chart data

--- Projects/Data-Visualization/chapter_16/mapping_global_data/eq_world_map.py
+++ Projects/Data-Visualization/chapter_16/mapping_global_data/eq_world_map.py
@@ -16,7 +16,12 @@ for eq_dict in all_eq_dicts:
     lons.append(lon)
     lats.append(lat)
 
-data = [Scattergeo(lon=lons, lat=lats)]
+# data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+}]
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
