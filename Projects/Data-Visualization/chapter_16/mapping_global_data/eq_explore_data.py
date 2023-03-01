1778bd9763bd4dada35283e26cbdc27fbfb47323
Naureen <naureen.imran123@yahoo.com>
2023-03-01 10:52:59 +0000

    Extract location data

--- Projects/Data-Visualization/chapter_16/mapping_global_data/eq_explore_data.py
+++ Projects/Data-Visualization/chapter_16/mapping_global_data/eq_explore_data.py
@@ -5,8 +5,15 @@ with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
-mags = []
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
 print(mags[:10])
+print(lats[:5])
+print(lons[:5])
