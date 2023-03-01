b09638d74618c4bdcbd36348ab36825135735eb8
Naureen <naureen.imran123@yahoo.com>
2023-03-01 11:02:32 +0000

    Build a world map

---
+++ Projects/Data-Visualization/chapter_16/mapping_global_data/eq_world_map.py
@@ -0,0 +1,23 @@
+import json
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+filename = 'Projects/Data-Visualization/chapter_16/mapping_global_data/data/eq_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
